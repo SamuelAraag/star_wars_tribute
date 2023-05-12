f096b91fe76b570fb845182e6c2e2bdcf3a05fa3
Samuel Araag <samuelaraag@MacBook-Air-de-Samuel.local>
2023-05-12 06:39:20 -0300

    criado evento para rotas nao existentes serem direcionadas para tela principal - home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { NavigationError, Router } from "@angular/router";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = "star_wars_tribute";
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    const rotaHome = "";
+
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationError) {
+        this.router.navigate([rotaHome]);
+      }
+    });
+  }
 }
